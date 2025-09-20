09f7e644209c784d4ae35b9c470c596b1a2b7599
Christopher Vestman <christophervestman@gmail.com>
2025-09-20 18:23:06 +0200

    initial frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import path from "node:path"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+})
