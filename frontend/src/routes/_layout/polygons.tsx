a3b475229dd9d4fd7bf2d07e338f1da60951a357
Christopher Vestman <christophervestman@gmail.com>
2025-10-06 22:05:29 +0200

    fix(leaflet): change mapstyle

--- frontend/src/routes/_layout/polygons.tsx
+++ frontend/src/routes/_layout/polygons.tsx
@@ -171,8 +171,10 @@ function Polygons() {
           style={{ height: '500px', width: '100%' }}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            subdomains='abcd'
+            maxZoom={20}
           />
           <MapResizer />
         </MapContainer>
