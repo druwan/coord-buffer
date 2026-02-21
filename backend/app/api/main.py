9cf7459236e82954183f858c19d482abee9353e0
Christopher Vestman <christophervestman@gmail.com>
2026-02-21 23:22:38 +0100

    fix: rm Item

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.routes import aip, items, login, polygons, private, users, utils
+from app.api.routes import aip, login, polygons, private, users, utils
 from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
-api_router.include_router(items.router)
 api_router.include_router(polygons.router)
 api_router.include_router(aip.router)
 
--- backend/app/models.py
+++ backend/app/models.py
@@ -53,7 +53,6 @@ class User(UserBase, table=True):
         default_factory=get_datetime_utc,
         sa_type=DateTime(timezone=True),  # type: ignore
     )
-    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
     polygons: list["Polygon"] = Relationship(
         back_populates="owner", cascade_delete=True
     )
@@ -67,47 +66,6 @@ class UserPublic(UserBase):
 
 class UsersPublic(SQLModel):
     data: list[UserPublic]
-    count: int
-
-
-# Shared properties
-class ItemBase(SQLModel):
-    title: str = Field(min_length=1, max_length=255)
-    description: str | None = Field(default=None, max_length=255)
-
-
-# Properties to receive on item creation
-class ItemCreate(ItemBase):
-    pass
-
-
-# Properties to receive on item update
-class ItemUpdate(ItemBase):
-    title: str | None = Field(default=None, min_length=1, max_length=255)
-
-
-# Database model, database table inferred from class name
-class Item(ItemBase, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    created_at: datetime | None = Field(
-        default_factory=get_datetime_utc,
-        sa_type=DateTime(timezone=True),  # type: ignore
-    )
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="items")
-
-
-# Properties to return via API, id is always required
-class ItemPublic(ItemBase):
-    id: uuid.UUID
-    owner_id: uuid.UUID
-    created_at: datetime | None = None
-
-
-class ItemsPublic(SQLModel):
-    data: list[ItemPublic]
     count: int
 
 
@@ -136,6 +94,7 @@ class NewPassword(SQLModel):
 class PolygonBase(SQLModel):
     title: str
     nameofarea: str
+    msid: int
     positionindicator: str
     buffer_size: int = Field(ge=1, lt=50)
     original_geometry: dict | None = Field(default=None, sa_column=Column(JSON))
